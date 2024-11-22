6702448d3d65cb012bee0197952bbb1d874111db
aryatrb <arya.torabi.deadarya@gmail.com>
2024-11-23 00:24:13 +0330

    Implement divide

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -4,6 +4,12 @@ public class Calculator {
     public double add(double a, double b) {
         return a + b;
     }
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
 
 public double subtract(double a, double b) {
