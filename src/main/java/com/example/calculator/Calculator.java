522522014c9b0302c586293344a03f355d3da430
ParJavadian <parjavadian@gmail.com>
2024-11-23 17:09:41 +0330

    Remove pom.xml from gitignore

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -4,18 +4,20 @@ public class Calculator {
     public double add(double a, double b) {
         return a + b;
     }
+
     public double divide(double a, double b) {
         if (b == 0) {
             throw new ArithmeticException("Cannot divide by zero");
         }
         return a / b;
     }
-}
 
-public double subtract(double a, double b) {
-    return a - b;
-}
 
-public double multiply(double a, double b) {
-    return a * b;
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
 }
